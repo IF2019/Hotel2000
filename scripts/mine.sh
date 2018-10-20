d88a47bdaa5f7c3b7c5652b0c6451e5de7f361e8
Unknown <hugues.vogel@hotmail.fr>
2018-10-20 21:42:15 +0200

    fix(scripts):  add theads parameter

--- scripts/mine.sh
+++ scripts/mine.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 ACCOUNT=$1
-ACCOUNT=${ACCOUNT:=1}
+ACCOUNT=${ACCOUNT:=0}
+THREAD=$2
+THREAD=${THREAD:=4}
 
 BLOCKCHAIN_NAME=bchotel2000
 BLOCKCHAIN_DIR=blockchain
 
 cd $( dirname "${BASH_SOURCE[0]}" )/../${BLOCKCHAIN_DIR}/${BLOCKCHAIN_NAME}
 
-echo "geth --etherbase $ACCOUNT --minerthreads=4 --mine --datadir ./"
-geth --etherbase $ACCOUNT --minerthreads=4 --mine --datadir ./
+echo "geth --etherbase $ACCOUNT --minerthreads=$THREAD --mine --datadir ./"
+geth --etherbase $ACCOUNT --minerthreads=$THREAD --mine --datadir ./
--- scripts/server.sh
+++ scripts/server.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 ACCOUNT=$1
-ACCOUNT=${ACCOUNT:=1}
+ACCOUNT=${ACCOUNT:=0}
+THREAD=$2
+THREAD=${THREAD:=1}
 
 BLOCKCHAIN_NAME=bchotel2000
 BLOCKCHAIN_DIR=blockchain
 
 cd $( dirname "${BASH_SOURCE[0]}" )/../${BLOCKCHAIN_DIR}/${BLOCKCHAIN_NAME}
 
-echo "geth --datadir=./ --rpcapi personal,db,eth,net,web3 --rpc --etherbase $ACCOUNT --minerthreads=1 --mine console"
+echo "geth --datadir=./ --rpcapi personal,db,eth,net,web3 --rpc --etherbase $ACCOUNT --minerthreads=$THREAD --mine console"
 
-geth  --etherbase $ACCOUNT --datadir=./ --rpcapi personal,db,eth,net,web3 --rpc --minerthreads=1 --mine console
+geth  --etherbase $ACCOUNT --datadir=./ --rpcapi personal,db,eth,net,web3 --rpc --minerthreads=$THREAD --mine console
