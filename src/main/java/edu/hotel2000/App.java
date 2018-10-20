2bd17b16682e355ccb5fe617565b0846adc98786
Unknown <hugues.vogel@hotmail.fr>
2018-10-20 19:40:04 +0200

    feat(contract): Add solidity basic contract

--- src/main/java/edu/hotel2000/App.java
+++ src/main/java/edu/hotel2000/App.java
@@ -1,13 +1,14 @@
 package edu.hotel2000;
 
+import edu.hotel2000.contract.Hotel2000;
 import org.apache.log4j.Logger;
-import org.web3j.crypto.CipherException;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.DefaultGasProvider;
 
-import java.io.IOException;
+import java.io.Console;
 
 public class App{
 
@@ -17,16 +18,18 @@ public class App{
 	private static final String DEPLOYER_FILE = Config.get("hotel2000.account.deploy.path");
 	private static final String WEB3J_URL = Config.get("web3jServer");
 
-	public static void main(String[] args) throws IOException, CipherException{
+	public static void main(String[] args) throws Exception{
 
 		logger.info("Create Web3j HTTP service");
 		Web3j web3j = Web3j.build(new HttpService(WEB3J_URL));
 		logger.debug("Create Web3j HTTP service Finish");
 
 
-		logger.debug("Create deployer credentials");
 		Credentials credentials = WalletUtils.loadCredentials(DEPLOYER_PASSPHRASE, DEPLOYER_FILE);
+		logger.info("Deployer Contract Hotel2000 from: " + DEPLOYER_ADDRESS);
+		Hotel2000 hotel2000 = Hotel2000.deploy(web3j, credentials, new DefaultGasProvider()).send();
 
+		logger.info("test: " + hotel2000.test().send());
 
 
 	}
---
+++ src/main/solidity/hotel2000.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.17;
+
+contract hotel2000 {
+
+	function test() public pure returns (string){
+		return "Test success";
+	}
+}
