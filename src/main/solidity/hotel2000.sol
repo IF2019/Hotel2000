951b60588d4e30f6326e84adcec7e8c7c1167ab0
Julianne Stingray <flat.bartender@gmail.com>
2018-10-22 08:44:12 +0200

    Some writing convetion changes

--- src/main/solidity/hotel2000.sol
+++ src/main/solidity/hotel2000.sol
@@ -1,65 +1,64 @@
 pragma solidity ^0.4.17;
 import {Lib} from "./lib.sol";
 
-contract Hotel2000{
+contract Hotel2000 {
 
-	uint reservationIdInc = 1;
-	mapping(uint => Lib.Resevation) reservations; // ResevationID => Reservation
+	uint bookingIdInc = 1;
+	mapping(uint => Lib.Booking) bookings; // ResevationID => Reservation
 	mapping(string => Lib.Hotel) hotels; // HotelCode  => Hotel
 
-	constructor() public{}
+	constructor() public {}
 
-	function initChambre(Lib.Chambre storage chambre)internal{
-		chambre.isset = true;
+	function initRoom(Lib.Room storage room) internal {
+		room.isset = true;
 	}
 
-	function initHotel(Lib.Hotel storage hotel,string _code, uint _nbChambres, uint _prix)internal{
-		hotel.code=_code;
-		hotel.nbChambres=_nbChambres;
-		hotel.createdAt=now;
-		hotel.prix=_prix;
-		for(uint i = 0; i<_nbChambres; i++){
-			initChambre(hotel.chambres[i]);
+	function initHotel(Lib.Hotel storage hotel, string _code, uint _nbRooms, uint _price) internal {
+		hotel.code      = _code;
+		hotel.nbRooms   = _nbRooms;
+		hotel.createdAt = now;
+		hotel.price     = _price;
+		for (uint i = 0; i < _nbRooms; i++) {
+			initRoom(hotel.rooms[i]);
 		}
-		hotel.isset=true;
+		hotel.isset = true;
 	}
 
-	function canBuildHotel(string _code, uint _nbChambres, uint _prix) public view returns(bool, string){
-		if(bytes(_code).length < 2 || bytes(_code).length > 8) return (false, "le code doit contenir entre 2 et 8 caractÃ©re");
-		if(hotels[_code].isset) return (false, "Code dÃ©ja Existant");
-		if(_nbChambres < 1) return (false, "nbChambre < 1");
-		if(_nbChambres > 10000) return (false, "trop de chambre!");
-		if(_prix < 1) return (false, "prix < 1");
+	function canBuildHotel(string _code, uint _nbRooms, uint _price) public view returns(bool, string) {
+		if (bytes(_code).length < 2 || bytes(_code).length > 8)
+            return (false, "the code must be between 2 and 8 characters long");
+		if (hotels[_code].isset) return (false, "this code already exists");
+		if (_nbRooms < 1)     return (false, "the hotel must have more than 1 room");
+		if (_nbRooms > 10000) return (false, "the hotel must have less than 10_000 rooms");
+		if (_price   < 1)     return (false, "the rooms must cost more than 1");
 		return (true, "");
 	}
 
-	function getHotel(string _code) public view returns(string, string, string, uint, address, uint){
-		require(hotels[_code].isset, "Hotel not found");
-		return(
-		hotels[_code].code,
-		hotels[_code].title,
-		hotels[_code].descitions,
-		hotels[_code].prix,
-		hotels[_code].proprio,
-		hotels[_code].nbChambres
-		);
+	function getHotel(string _code) public view returns(string, string, string, uint, address, uint) {
+		require(hotels[_code].isset, "hotel not found");
+        return(
+            hotels[_code].code,
+            hotels[_code].title,
+            hotels[_code].description,
+            hotels[_code].price,
+            hotels[_code].owner,
+            hotels[_code].nbRooms
+        );
 	}
 
-	function buildHotel(string _code, uint _nbChambres, uint _prix) public{
-		bool canBuil;
+	function buildHotel(string _code, uint _nbRooms, uint _price) public {
+		bool          canBuild;
 		string memory message;
-		(canBuil, message) = canBuildHotel(_code, _nbChambres, _prix);
-		require(canBuil, message);
-		initHotel(hotels[_code], _code, _nbChambres, _prix);
-
+		(canBuild, message) = canBuildHotel(_code, _nbRooms, _price);
+		require(canBuild, message);
+		initHotel(hotels[_code], _code, _nbRooms, _price);
 	}
 
-
-	function timestampToDaystamp(uint timestamp) pure public returns(uint){
+	function timestampToDaystamp(uint timestamp) pure public returns(uint) {
 		return timestamp / 86400;
 	}
 
-	function test() public pure returns (string){
-		return "Test success";
+	function test() public pure returns (string) {
+		return "test success";
 	}
 }
--- src/main/solidity/lib.sol
+++ src/main/solidity/lib.sol
@@ -1,32 +1,32 @@
 pragma solidity ^0.4.17;
 
-library Lib{
-	struct Resevation{
-		bool isset; // Should by true
-		uint id;
-		uint createdAt; // Timestamp
-		uint debut; // Daystamp
-		uint fin; // Daystamp
-		string hotelCode;
-		uint chambre; // Index
-		uint prix;
+library Lib {
+	struct Booking {
+		bool    isset;      // Should be true
+		uint    id;
+		uint    createdAt;  // Timestamp
+		uint    start;      // Daystamp
+		uint    end;        // Daystamp
+		string  hotelCode;
+		uint    room;       // Index
+		uint    price;
 		address client;
 	}
 
-	struct Chambre{
-		bool isset; // Should by true
-		mapping(uint => uint) reservations; // Daystamp => ResevationID
+	struct Room {
+		bool isset; // Should be true
+		mapping(uint => uint) bookings; // Daystamp => BookingID
 	}
 
-	struct Hotel{
-		bool isset; // Should by true
-		string code;
-		uint createdAt; // Timestamp
-		string title;
-		string descitions;
-		uint prix; // Prix par nuit
-		mapping(uint => Chambre) chambres;
-		uint nbChambres;
-		address proprio;
+	struct Hotel {
+		bool    isset;      // Should be true
+		string  code;
+		uint    createdAt;  // Timestamp
+		string  title;
+		string  description;
+		uint    price;      // Price per night
+		uint    nbRooms;
+		address owner;
+		mapping(uint => Room) rooms;
 	}
 }
