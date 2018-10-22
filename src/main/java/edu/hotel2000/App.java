2f63a551332df45ee2488efe19cc9506285eda09
Unknown <hugues.vogel@hotmail.fr>
2018-10-22 10:45:40 +0200

    feat(create): add java commande

--- src/main/java/edu/hotel2000/App.java
+++ src/main/java/edu/hotel2000/App.java
@@ -6,6 +6,7 @@ import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tuples.generated.Tuple2;
 import org.web3j.tx.gas.DefaultGasProvider;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class App{
 	private static final Logger logger = Logger.getLogger(App.class);
 	private static final String WEB3J_URL = Config.get("web3jServer");
 	private static Web3j web3j;
+	private static String hotel2000ContractAddress;
 
 	public static void main(String[] args) throws Exception{
 
@@ -31,6 +33,8 @@ public class App{
 		Credentials credentials = AccountService.findFromConfig("deploy");
 		logger.info("Deployer Contract Hotel2000 from: " + credentials.getAddress());
 		Hotel2000 hotel2000 = Hotel2000.deploy(web3j, credentials, new DefaultGasProvider()).send();
+		logger.info("Contract Hotel2000 deployed! ContractAddress: " + hotel2000.getContractAddress());
+		hotel2000ContractAddress = hotel2000.getContractAddress();
 
 		logger.info("test: " + hotel2000.test().send());
 
@@ -38,21 +42,65 @@ public class App{
 	}
 
 	private static void showBalance(String account) throws IOException{
-		String address = AccountService.findFromConfigOption("deploy")
-				.map(Credentials::getAddress)
-				.orElseGet(() -> {
-					logger.info("Account " + account + " not found");
-					return account;
-				});
+		String address = accountNameToAddress(account);
 		if(!address.substring(0, 2).equals("0x")) address = "0x" + address;
 		BigInteger wei = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).send().getBalance();
 		logger.info(account + " has " + wei + " wei");
-		logger.info(account + " has " + wei.divide(new BigInteger("10000000000000000")).intValue()/100f + " eth");
+		logger.info(account + " has " + wei.divide(BigInteger.valueOf(10000000000000000L)).intValue() / 100f + " eth");
+	}
+
+	private static String accountNameToAddress(String accountName){
+		return AccountService.findFromConfigOption(accountName)
+				.map(Credentials::getAddress)
+				.orElseGet(() -> {
+					logger.debug("Account " + accountName + " not found");
+					return accountName;
+				});
+	}
+
+	private static void canCreateHotel(String accountName, String code, int nbRoom, BigInteger prix) throws Exception{
+		Credentials account = AccountService.findFromConfigOption(accountName)
+				.orElseThrow(() -> new RuntimeException("Account " + accountName + " not found"));
+		Hotel2000 hotel2000 = Hotel2000.load(hotel2000ContractAddress, web3j, account, new DefaultGasProvider());
+		Tuple2<Boolean, String> res = hotel2000.canBuildHotel(code, BigInteger.valueOf(nbRoom), prix).send();
+		if(res.getValue1()){
+			logger.info(accountName + " can create " + code + " hotel");
+		}else{
+			logger.info(accountName + " can't create " + code + " hotel: " + res.getValue2());
+		}
+
+	}
+
+	private static void createHotel(String accountName, String code, int nbRoom, BigInteger prix) throws Exception{
+		Credentials account = AccountService.findFromConfigOption(accountName)
+				.orElseThrow(() -> new RuntimeException("Account " + accountName + " not found"));
+		Hotel2000 hotel2000 = Hotel2000.load(hotel2000ContractAddress, web3j, account, new DefaultGasProvider());
+		try{
+			hotel2000.buildHotel(code, BigInteger.valueOf(nbRoom), prix).send();
+			logger.info("hotel " + accountName + " created");
+		}catch(Exception e){
+			logger.error("fail create hotel", e);
+		}
+
+
 	}
 
 	private static void receiveCommand(String[] commande) throws Exception{
+
+
 		String help = Config.get("hotel2000.commands.help");
-		if(commande[0].isEmpty())return;
+		if(commande[0].isEmpty()) return;
+		if(commande[0].equals("?")
+				|| commande[0].equalsIgnoreCase("h")
+				|| commande[0].equalsIgnoreCase("help")
+		){
+			if(commande.length > 1){
+				System.out.println(Config.get("hotel2000.commands." + commande[1] + ".help", "Command  '" + commande[1] + "' didn't have help"));
+				return;
+			}
+			System.out.println(help);
+			return;
+		}
 		if(commande[0].equalsIgnoreCase("balance")){
 			if(commande.length < 2){
 				System.out.println(Config.get("hotel2000.commands.balance.help", help));
@@ -61,7 +109,28 @@ public class App{
 			showBalance(commande[1]);
 			return;
 		}
-		System.err.println("Command '" + commande[0] + "' unknown");
+		if(commande[0].equalsIgnoreCase("hotel")){
+			help = Config.get("hotel2000.commands.hotel.help", help);
+			if(commande.length < 2){
+				System.out.println();
+				return;
+			}else if(commande[1].equalsIgnoreCase("create")){
+				if(commande.length < 6){
+					System.out.println(Config.get("hotel2000.commands.hotel.create.help", help));
+					return;
+				}
+				createHotel(commande[2], commande[3], Integer.parseInt(commande[4]), new BigInteger(commande[5]));
+				return;
+			}else if(commande[1].equalsIgnoreCase("canCreate")){
+				if(commande.length < 6){
+					System.out.println(Config.get("hotel2000.commands.hotel.create.help", help));
+					return;
+				}
+				canCreateHotel(commande[2], commande[3], Integer.parseInt(commande[4]), new BigInteger(commande[5]));
+				return;
+			}
+		}
+		System.out.println("Command '" + commande[0] + "' unknown");
 		System.out.println(help);
 	}
 }
